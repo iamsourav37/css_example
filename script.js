093f57987cbf04ff6cf2fb5819deacb13694fa23
Sourav Ganguly <souravganguly108@gmail.com>
2021-03-18 10:27:26 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+
+document.getElementById("demo").innerHTML = "Hello world, from Javascript";
+
+var myName = "Sourav Ganguly";
+
+document.getElementById("showName").innerHTML = myName;
+
+console.log(myName);
+
+x = 10;
+y = 12;
+
+console.log(x+y);
+
+const PI = 3.14;
+
+console.log("PI values is : ")
